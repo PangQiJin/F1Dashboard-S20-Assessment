baca86c4f0d71392c47b6d775839bed04eb19be1
PangQiJin <jinsonpangqj@gmail.com>
2026-09-03 18:13:52 +0800

    Github and bite deployment path sync so published site not white screen

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
     react(),
   ],
 
-  // GitHub Pages hosts this project under /F1Dashboard/.
+  // GitHub Pages hosts this project under
+  // /F1Dashboard-S20-Assessment/.
   // This ensures generated CSS, JavaScript and asset paths
   // point to the correct location after deployment.
-  base: '/F1Dashboard/',
+  base: '/F1Dashboard-S20-Assessment/',
 })
